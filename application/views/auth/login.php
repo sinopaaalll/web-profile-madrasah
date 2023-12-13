166553ad18e3acfe7dfbc24c874c24be84b935bb
sinopaaalll <nrizqullah825@gmail.com>
2023-12-14 06:21:26 +0700

    fix footer

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -33,7 +33,9 @@
                 <div class="row">
                     <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
                         <div class="login-brand">
-                            <img src="<?= base_url('assets/user/') ?>images/Logo.png" alt="logo" width="100" class=" ">
+                            <a href="<?= base_url('home') ?>">
+                                <img src="<?= base_url('assets/user/') ?>images/Logo.png" alt="logo" width="100" class=" ">
+                            </a>
                         </div>
 
                         <div class="card card-primary">
@@ -86,7 +88,7 @@
                             Don't have an account? <a href="auth-register.html">Create One</a>
                         </div> -->
                         <div class="simple-footer">
-                            Copyright &copy; 2023
+                            Copyright &copy; 2023. Dwi Fristtikasari
                         </div>
                     </div>
                 </div>
